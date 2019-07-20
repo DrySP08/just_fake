1007b1ee886d0a941e3c69cb1483c226d87c44fb
DrySP08 <53125449+DrySP08@users.noreply.github.com>
2019-07-21 03:09:49 +0700

    Add files via upload

---
+++ Dry_SP08/subdo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+clear
+echo " =================== Welcome To ===================";
+echo 'Just- _____     _            _____           _     ';
+echo '     |  ___|_ _| | _____    |_   _|__   ___ | | ___  ';
+echo '     | |_ / _` | |/ / _ \     | |/ _ \ / _ \| |/ __| ';
+echo '     |  _| (_| |   <  __/_____| | (_) | (_) | |\__ \ ';
+echo '     |_|  \__,_|_|\_\___|_____|_|\___/ \___/|_||___/ v.1.0 ';
+echo ' ---------------- Coded By Dry_SP08 --------------';
+echo '';
+read -p "Domain : http://" domain
+read -p "Delay? (1-5 ) :" delay
+read -p "Jumlah? (1-100000):" jumlah
+read -p "Output (test.txt) :" output
+echo "[+] Generating..."
+count=1
+while [ $count -le $jumlah ]
+do
+printf "http://$RANDOM.$domain\n" >> $output
+echo "[$count] Ganerate => data/$output"
+sleep $delay
+(( count++ ))
+done
+echo "[+] Success"
+
---
+++ fake.sh
@@ -0,0 +1,281 @@
+#!/bin/bash
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+# echo "starting tools... /";
+# sleep 0.1;
+# clear
+# echo 'Starting tools... -';
+# sleep 0.1;
+# clear
+# echo 'sTarting tools... \';
+# sleep 0.1;
+# clear
+# echo "stArting tools... /";
+# sleep 0.1;
+# clear
+# echo 'staRting tools... -';
+# sleep 0.1;
+# clear
+# echo 'starTing tools... \';
+# sleep 0.1;
+# clear
+# echo "startIng tools... /";
+# sleep 0.1;
+# clear
+# echo "startiNg tools... -";
+# sleep 0.1;
+# clear
+# echo 'startinG tools... \';
+# sleep 0.1;
+# clear
+# echo 'starting Tools... -';
+# sleep 0.1;
+# clear
+# echo "starting tOols... /";
+# sleep 0.1;
+# clear
+# echo 'starting toOls... -';
+# sleep 0.1;
+# clear
+# echo 'starting tooLs... \';
+# sleep 0.1;
+# clear
+# echo "starting toolS... /";
+# sleep 0.1;
+# clear
+# echo "starting tools... /";
+# sleep 0.1;
+# clear
+# echo 'Starting tools... -';
+# sleep 0.1;
+# clear
+# echo 'sTarting tools... \';
+# sleep 0.1;
+# clear
+# echo "stArting tools... /";
+# sleep 0.1;
+# clear
+# echo 'staRting tools... -';
+# sleep 0.1;
+# clear
+# echo 'starTing tools... \';
+# sleep 0.1;
+# clear
+# echo "startIng tools... /";
+# sleep 0.1;
+# clear
+# echo "startiNg tools... -";
+# sleep 0.1;
+# clear
+# echo 'startinG tools... \';
+# sleep 0.1;
+# clear
+# echo 'starting Tools... -';
+# sleep 0.1;
+# clear
+# echo "starting tOols... /";
+# sleep 0.1;
+# clear
+# echo 'starting toOls... -';
+# sleep 0.1;
+# clear
+# echo 'starting tooLs... \';
+# sleep 0.1;
+# clear
+# echo "starting toolS... /";
+# sleep 0.1;
+# clear
+# echo "starting tools... /";
+# sleep 0.1;
+# clear
+# echo 'Starting tools... -';
+# sleep 0.1;
+# clear
+# echo 'sTarting tools... \';
+# sleep 0.1;
+# clear
+# echo "stArting tools... /";
+# sleep 0.1;
+# clear
+# echo 'staRting tools... -';
+# sleep 0.1;
+# clear
+# echo 'starTing tools... \';
+# sleep 0.1;
+# clear
+# echo "startIng tools... /";
+# sleep 0.1;
+# clear
+# echo "startiNg tools... -";
+# sleep 0.1;
+# clear
+# echo 'startinG tools... \';
+# sleep 0.1;
+# clear
+# echo 'starting Tools... -';
+# sleep 0.1;
+# clear
+# echo "starting tOols... /";
+# sleep 0.1;
+# clear
+# echo 'starting toOls... -';
+# sleep 0.1;
+# clear
+# echo 'starting tooLs... \';
+# sleep 0.1;
+# clear
+# echo "starting toolS... /";
+# sleep 0.1;
+# clear
+# echo "starting tools... /";
+# sleep 0.1;
+# clear
+# echo 'Starting tools... -';
+# sleep 0.1;
+# clear
+# echo 'sTarting tools... \';
+# sleep 0.1;
+# clear
+# echo "stArting tools... /";
+# sleep 0.1;
+# clear
+# echo 'staRting tools... -';
+# sleep 0.1;
+# clear
+# echo 'starTing tools... \';
+# sleep 0.1;
+# clear
+# echo "startIng tools... /";
+# sleep 0.1;
+# clear
+# echo "startiNg tools... -";
+# sleep 0.1;
+# clear
+# echo 'startinG tools... \';
+# sleep 0.1;
+# clear
+# echo 'starting Tools... -';
+# sleep 0.1;
+# clear
+# echo "starting tOols... /";
+# sleep 0.1;
+# clear
+# echo 'starting toOls... -';
+# sleep 0.1;
+# clear
+# echo 'starting tooLs... \';
+# sleep 0.1;
+# clear
+# echo "starting toolS... /";
+# sleep 0.1;
+# clear
+echo " =================== Welcome To ===================";
+echo 'Just- _____     _            _____           _     ';
+echo '     |  ___|_ _| | _____    |_   _|__   ___ | | ___  ';
+echo '     | |_ / _` | |/ / _ \     | |/ _ \ / _ \| |/ __| ';
+echo '     |  _| (_| |   <  __/_____| | (_) | (_) | |\__ \ ';
+echo '     |_|  \__,_|_|\_\___|_____|_|\___/ \___/|_||___/ v.1.0 ';
+echo ' ---------------- Coded By Dry_SP08 --------------';
+echo '';
+echo 'Pesan : Dilarang Menggunakan Tools Ini! Kalau Lu Baperan !';
+echo '    	  Jangan Bawa Nama Team Sembarangan Ijin Dulu Ke LEAD LU !. Gunakan Hanya Saat Di Butuhkan Saja !';
+echo '';
+echo 'Pilih Opsi:';
+echo '[1] Generate Subdomain';
+echo '[2] Mass Submit Def.ID';
+#echo '[3] Auto Tebas (Spoof)';
+echo ''
+read -p "just_fake@tools:~#" command
+if [ $command -eq 1 ];
+	then
+		cd Dry_SP08/ && chmod +x subdo.sh && ./subdo.sh
+elif [ $command -eq 2 ];
+	then
+		cd Dry_SP08/ && chmod +x submit.sh && ./submit.sh
+#elif [ $command -eq 3 ];
+#	then
+#		cd data/ && chmod +x spoof.sh && ./spoof.sh
+fi
